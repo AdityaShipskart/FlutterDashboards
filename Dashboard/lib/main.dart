1f7503b6b4591f77045cc3cd1725b646f01ddab1
YYash-Dixit <yash@Shipskart1.onmicrosoft.com>
2025-11-17 11:33:11 +0530

    add a switch statements

--- Dashboard/lib/main.dart
+++ Dashboard/lib/main.dart
@@ -1,5 +1,7 @@
 // import 'package:flutte_design_application/pages/dashboard_hub.dart';
 import 'package:flutte_design_application/pages/new-dashboards/vendor_dashboard.dart';
+import 'package:flutte_design_application/pages/new-dashboards/catalogue_dashboard_new.dart';
+import 'package:flutte_design_application/pages/new-dashboards/smc_dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,8 +20,11 @@ class MyApp extends StatefulWidget {
   }
 }
 
+enum DashboardType { catalogue, smc, vendor }
+
 class MyAppState extends State<MyApp> {
   ThemeMode _themeMode = ThemeMode.light;
+  DashboardType _currentDashboard = DashboardType.vendor;
 
   void toggleTheme() {
     setState(() {
@@ -35,6 +40,23 @@ class MyAppState extends State<MyApp> {
     setState(() {
       _themeMode = mode;
     });
+  }
+
+  void switchDashboard(DashboardType dashboard) {
+    setState(() {
+      _currentDashboard = dashboard;
+    });
+  }
+
+  Widget _getCurrentDashboard() {
+    switch (_currentDashboard) {
+      case DashboardType.catalogue:
+        return const CatalogueDashboard();
+      case DashboardType.smc:
+        return const SMCDashboard();
+      case DashboardType.vendor:
+        return const VendorDashboard();
+    }
   }
 
   @override
@@ -54,7 +76,75 @@ class MyAppState extends State<MyApp> {
         useMaterial3: true,
       ),
       themeMode: _themeMode,
-      home: const VendorDashboard(),
+      home: DashboardSelector(
+        currentDashboard: _currentDashboard,
+        onDashboardChanged: switchDashboard,
+        child: _getCurrentDashboard(),
+      ),
     );
+  }
+}
+
+class DashboardSelector extends StatelessWidget {
+  final DashboardType currentDashboard;
+  final Function(DashboardType) onDashboardChanged;
+  final Widget child;
+
+  const DashboardSelector({
+    super.key,
+    required this.currentDashboard,
+    required this.onDashboardChanged,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_getDashboardTitle(currentDashboard)),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          PopupMenuButton<DashboardType>(
+            onSelected: onDashboardChanged,
+            icon: const Icon(Icons.dashboard),
+            itemBuilder: (BuildContext context) =>
+                <PopupMenuEntry<DashboardType>>[
+                  const PopupMenuItem<DashboardType>(
+                    value: DashboardType.catalogue,
+                    child: Text('Catalogue Dashboard'),
+                  ),
+                  const PopupMenuItem<DashboardType>(
+                    value: DashboardType.smc,
+                    child: Text('SMC Dashboard'),
+                  ),
+                  const PopupMenuItem<DashboardType>(
+                    value: DashboardType.vendor,
+                    child: Text('Vendor Dashboard'),
+                  ),
+                ],
+          ),
+          IconButton(
+            onPressed: () => MyApp.of(context)?.toggleTheme(),
+            icon: Icon(
+              Theme.of(context).brightness == Brightness.light
+                  ? Icons.dark_mode
+                  : Icons.light_mode,
+            ),
+          ),
+        ],
+      ),
+      body: child,
+    );
+  }
+
+  String _getDashboardTitle(DashboardType dashboard) {
+    switch (dashboard) {
+      case DashboardType.catalogue:
+        return 'Catalogue Dashboard';
+      case DashboardType.smc:
+        return 'SMC Dashboard';
+      case DashboardType.vendor:
+        return 'Vendor Dashboard';
+    }
   }
 }
